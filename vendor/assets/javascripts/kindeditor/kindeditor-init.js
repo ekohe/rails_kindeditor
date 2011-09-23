dedf7baf7418360682742e8e383a352510bf68ca
JerryShen <hansay99@gmail.com>
2011-09-23 21:55:39 +0800

    add two kindeditor input

--- vendor/assets/javascripts/kindeditor/kindeditor-init.js
+++ vendor/assets/javascripts/kindeditor/kindeditor-init.js
@@ -1,5 +1,13 @@
 KindEditor.ready(function(K) {
-	K.create('#kindeditor_id', {
+	K.create('#kindeditor_input', {
+		width: "80%",
+		allowFileManager: true,
+		langType: "zh_CN",
+		uploadJson: '/kindeditor/upload',
+		fileManagerJson: '/kindeditor/filemanager'
+	});
+	
+	K.create('#kindeditor_input_s', {
 		width: "80%",
 		allowFileManager: true,
 		langType: "zh_CN",
