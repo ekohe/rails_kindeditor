8d6071fbf54edc8ec61a5d6874b541d0fe987022
JerryShen <hansay99@gmail.com>
2012-01-08 17:19:51 +0800

    add small formar

--- vendor/assets/javascripts/kindeditor/kindeditor-init.js
+++ vendor/assets/javascripts/kindeditor/kindeditor-init.js
@@ -1,17 +1,25 @@
 KindEditor.ready(function(K) {
-	K.create('#kindeditor_input', {
-		width: "80%",
-		allowFileManager: true,
-		langType: "zh_CN",
-		uploadJson: '/kindeditor/upload',
-		fileManagerJson: '/kindeditor/filemanager'
-	});
-	
-	K.create('#kindeditor_input_s', {
-		width: "80%",
-		allowFileManager: true,
-		langType: "zh_CN",
-		uploadJson: '/kindeditor/upload',
-		fileManagerJson: '/kindeditor/filemanager'
-	});
+  K.create('#kindeditor_input', {
+    width: "80%",
+    allowFileManager: true,
+    langType: "zh_CN",
+    uploadJson: '/kindeditor/upload',
+    fileManagerJson: '/kindeditor/filemanager'
+  });
+
+  K.create('#kindeditor_input_s', {
+    width: "80%",
+    allowFileManager: true,
+    langType: "zh_CN",
+    uploadJson: '/kindeditor/upload',
+    fileManagerJson: '/kindeditor/filemanager'
+  });
+  
+  K.create('#kindeditor_input_small', {
+    width: "60%",
+    allowFileManager: true,
+    langType: "zh_CN",
+    uploadJson: '/kindeditor/upload',
+    fileManagerJson: '/kindeditor/filemanager'
+  });
 });
